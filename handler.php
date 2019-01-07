c3397900b1e3fd11112b8dab3729334ad352c88a
Geoffrey Hoffman <geoffrey.hoffman@gmail.com>
2019-01-07 09:46:46 -0700

    Removed special characters

--- handler.php
+++ handler.php
@@ -1,7 +1,7 @@
 <?php
 function hello($eventData) : array
 {
-    $response = ['msg'·=>·'hello·from·PHP '·.·PHP_VERSION];
+    $response = ['msg' => 'hello from PHP '.PHP_VERSION];
     $response['eventData'] = $eventData;
     $response['request'] = $_REQUEST;
     return $response;
