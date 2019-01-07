1af5a53da58f0c9a8fd769fc7b83933a5171804c
Geoffrey Hoffman <geoffrey.hoffman@gmail.com>
2019-01-07 11:31:10 -0700

    Setting  up for taking POST values

--- handler.php
+++ handler.php
@@ -3,6 +3,7 @@ function hello($eventData) : array
 {
     $response = ['msg' => 'hello from PHP '.PHP_VERSION];
     $response['eventData'] = $eventData;
-    $response['request'] = $_REQUEST;
+    $data = json_decode($eventData);
+    $response['data'] = $data;
     return $response;
 }
